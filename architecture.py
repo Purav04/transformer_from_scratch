8a2fb539845ac4850faaa2f5c77b9bae5f09f6da
Purav04 <puravpatel408@gmail.com>
2025-02-16 21:04:02 +0530

    Transformer architecture

--- architecture.py
+++ architecture.py
@@ -53,7 +53,7 @@ class PositionWiseFeedForward(nn.Module):
         self.relu = nn.ReLU()
 
     def forward(self, x):
-        return self.fc2(self.relu(self.fc1(c)))
+        return self.fc2(self.relu(self.fc1(x)))
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_seq_length):
